972153bb5c0abfc4a1545074a4551e226db31742
Sypth <105406568+Sypth@users.noreply.github.com>
2024-08-27 17:00:15 +0800

    :wrench: Making URLs more dynamic and adding a Qodana Linter for checking errors and bugs inside the project.

--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,27 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the challenges index.")
+def monthly_challenge_by_number(request, month):
+    if month == 1:
+        challenge_text = "Eat no meat for the entire month!"
+    elif month == 2:
+        challenge_text = "Walk for at least 20 minutes every day!"
+    elif month == 3:
+        challenge_text = "Learn Django for at least 20 minutes every day!"
+    else:
+        return HttpResponseNotFound("This month is not supported!")
+
+    return HttpResponse(challenge_text)
+
+def monthly_challenges(request, month):
+    if month == "january":
+        challenge_text = "Eat no meat for the entire month!"
+    elif month =="february":
+        challenge_text = "Walk for at least 20 minutes every day!"
+    elif month == "march":
+        challenge_text = "Learn Django for at least 20 minutes every day!"
+    else:
+        return HttpResponseNotFound("This month is not supported!")
+
+    return HttpResponse(challenge_text)
